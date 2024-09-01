fe3ff17ffcb4f05c4a398884da465a30f8b751ef
abhishek6969 <sunkaleabhishek@gmail.com>
2024-09-01 12:03:43 +0530

    improvement

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_maintenance_configuration" "example" {
+resource "azurerm_maintenance_configuration" "maintenance_configuration_id" {
   name                = var.maintenance_configuration_name
   resource_group_name = var.maintenance_configuration_resource_group
   location            = var.maintenance_configuration_location
